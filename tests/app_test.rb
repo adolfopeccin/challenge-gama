17009d67f475016e0db6714d48de63ca0e420348
Adolfo Peccin <adolfopeccin@gmail.com>
2017-01-28 15:24:09 -0200

    hu3hu3

--- tests/app_test.rb
+++ tests/app_test.rb
@@ -14,4 +14,10 @@ class AppTest < Minitest::Test
     get '/'
     assert_match /Busca de Endereço/, last_response.body
   end
+
+  def test_home_busca_de_endereco
+  	endereco = 'Rua Casa do Ator, 294'.gsub(' ', '+')
+    get '/', url: endereco
+    assert_match /Vila Olimpia/, last_response.body
+  end
 end
